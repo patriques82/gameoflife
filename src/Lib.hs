e7fd5f6c9da999dac61c40065b156465e4f049e2
Patrik Nygren <patriknygren82@gmail.com>
2019-06-06 10:36:53 +0200

    Added new Rule type to DRY up some of the logic

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,10 +4,9 @@ module Lib
     ) where
 
 import qualified Data.Map.Strict as M
-
 import Data.List (intercalate)
 import Data.List.Split (chunksOf)
-import System.Random (randomRs, newStdGen)
+import System.Random (randomRs, newStdGen, RandomGen)
 import Control.Monad (join, void)
 import Control.Concurrent (threadDelay)
 
@@ -22,16 +21,17 @@ data GameState = GameState { gWidth :: Int
                            , grid :: M.Map Pos Cell
                            }
 
-instance Semigroup GameState where
-  a <> b = GameState { gWidth = max (gWidth a) (gWidth b)
-                     , grid = grid a <> grid b
-                     }
-
 instance Show GameState where
   show (GameState w g) =
     intercalate "\n" . chunksOf w . join . fmap show . M.elems $ g
 
 data Cell = Live | Dead
+  deriving Eq
+
+instance Semigroup Cell where
+  Live <> _    = Live
+  _ <> Live    = Live
+  Dead <> Dead = Dead
 
 instance Show Cell where
   show Live = "X"
@@ -44,11 +44,19 @@ type Pos = Int
 run :: Seed -> IO ()
 run s = void $ initGame s >>= loop
 
+initGame :: Seed -> IO GameState
+initGame s = newStdGen >>= \g ->
+  return $ GameState (width s) (alive g s <> dead s)
+
 loop :: GameState -> IO GameState
 loop g = do
+  display g
+  loop (tick g)
+
+display :: GameState -> IO ()
+display g = do
   putStrLn (ansiClearScreen ++ ansiGoto 1 1 ++ show g)
   threadDelay 1000000
-  loop (tick g)
 
 ansiClearScreen :: String
 ansiClearScreen = "\ESC[2J"
@@ -56,57 +64,50 @@ ansiClearScreen = "\ESC[2J"
 ansiGoto :: Int -> Int -> String
 ansiGoto x y = "\ESC[" ++ show y ++ ";" ++ show x ++ "H"
 
-initGame :: Seed -> IO GameState
-initGame s = do
-  g <- newStdGen
-  let total = width s * height s
-      alive = M.fromList $ take (liveCells s) $ ls
-      ls = randomRs (0, total) g `zip` repeat Live
-      dead = M.fromList $ take (total - liveCells s) $ ds
-      ds = [(p, c) | p <- [1..total], c <- repeat Dead]
-  return $ GameState (width s) (alive `M.union` dead)
+total s = width s * height s
+
+--
+
+alive :: RandomGen a => a -> Seed -> M.Map Pos Cell
+alive g s = M.fromList $ take (liveCells s) ls
+  where ls = randomRs (0, total s) g `zip` repeat Live
+
+dead :: Seed -> M.Map Pos Cell
+dead s = M.fromList $ take (total s - liveCells s) ds
+  where ds = [(p, c) | p <- [1..total s], c <- repeat Dead]
 
 -- Logic
 
-tick :: GameState -> GameState
-tick s = tickLiving s <> tickDead s
-
-tickLiving :: GameState -> GameState
-tickLiving (GameState w m) = GameState w $ M.fromList $ M.foldrWithKey' f [] m
-  where f pos cell xs = let n = neighbours w pos m
-                        in if rule2 cell n || rule4 cell n
-                              then (pos, Live):xs
-                              else xs
-
-tickDead :: GameState -> GameState
-tickDead (GameState w m) = GameState w $ M.fromList $ M.foldrWithKey' f [] m
-  where f pos cell xs = let n = neighbours w pos m
-                        in if rule1 cell n || rule3 cell n
-                              then (pos, Dead):xs
-                              else xs
-
 -- 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-rule1 :: Cell -> [(Pos, Cell)] -> Bool
-rule1 Live = (< 2) . length . filter (\(_, c) -> isAlive c)
-rule1 Dead = const False
-
 -- 2. Any live cell with two or three live neighbours lives on to the next generation.
-rule2 :: Cell -> [(Pos, Cell)] -> Bool
-rule2 Live = (> 1) . length . filter (\(_, c) -> isAlive c)
-rule2 Dead = const False
-
 -- 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-rule3 :: Cell -> [(Pos, Cell)] -> Bool
-rule3 Live = (> 3) . length . filter (\(_, c) -> isAlive c)
-rule3 Dead = const False
-
 -- 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-rule4 :: Cell -> [(Pos, Cell)] -> Bool
-rule4 Dead = (==) 3 . length . filter (\(_, c) -> isAlive c)
-rule4 Live = const False
+tick :: GameState -> GameState
+tick s = apply [ mkRule Live (< 2) Dead
+               , mkRule Live (\n -> n == 2 || n == 3) Live
+               , mkRule Live (> 3) Dead
+               , mkRule Dead ((==) 3) Live
+               ] s
 
-neighbours :: Int -> Pos -> M.Map Pos Cell -> [(Pos, Cell)]
-neighbours w p m = M.toList $ M.intersection m d
+newtype Rule = Rule (Cell -> [Cell] -> Maybe Cell)
+
+apply :: [Rule] -> GameState -> GameState
+apply rs (GameState w m) = GameState w . M.fromList $ M.foldrWithKey' f [] m
+  where f pos cell xs = let cs = neighbours w pos m
+                        in case mconcat (map (\(Rule f) -> f cell cs) rs) of -- ?
+                             Just c  -> (pos, c):xs
+                             Nothing -> xs
+
+
+
+mkRule :: Cell -> ([Cell] -> Bool) -> Cell -> Rule
+mkRule start pred end = Rule $ \c' cs ->
+  if start == c' && (pred . length . filter isAlive $ cs)
+     then Just end
+     else Nothing
+
+neighbours :: Int -> Pos -> M.Map Pos Cell -> [Cell]
+neighbours w p m = M.elems $ M.intersection m d
   where
     d = M.fromList [ (p-1-w, ()), (p-w, ()), (p+1-w, ())
                    , (p-1, ()),              (p+1, ())
@@ -115,5 +116,3 @@ neighbours w p m = M.toList $ M.intersection m d
 isAlive :: Cell -> Bool
 isAlive Live = True
 isAlive Dead = False
-
-
